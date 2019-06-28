c7d948ca102071f543059a644e99ef2cb43832f8
zph <839726650@qq.com>
2019-06-28 10:20:21 +0800

    3

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -8,11 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
-<<<<<<< HEAD
-	//id
-=======
-	//注解
->>>>>>> branch 'master' of https://github.com/zph839726650/local_repo
+
 	private Integer id;
 	private String name;
 	private Integer number;
